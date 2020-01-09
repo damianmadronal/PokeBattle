c564985ec0f44e3043e4f6e867d629b3ce621e68
Damian Madronal <munoz@avant.nl>
2020-01-09 15:51:47 +0100

    changed hp back to normal

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 require 'Pokemon.php';
 
 
-$pikachu = new Pokemon('Pikachu', 'Pickachu', 'Lightning', 600, [['Electric Ring', 50], ['Pika Punch', 20]], 'Fire', 1.5, 'Fighting', 20);
+$pikachu = new Pokemon('Pikachu', 'Pickachu', 'Lightning', 60, [['Electric Ring', 50], ['Pika Punch', 20]], 'Fire', 1.5, 'Fighting', 20);
 
-$charmeleon = new Pokemon('Charmeleon', 'Chermelon', 'Fire', 600, [['Head Butt', 10], ['Flare', 30]], 'Water', 2, 'Lightning', 10);
+$charmeleon = new Pokemon('Charmeleon', 'Chermelon', 'Fire', 60, [['Head Butt', 10], ['Flare', 30]], 'Water', 2, 'Lightning', 10);
 
 
 $pikachu->battleMove($charmeleon, $pikachu->attacks[rand(0, 1)]);
