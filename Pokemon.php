1b12e9f72fed03ea063ffe8b67c6cd2d858bf0a3
Damian Madronal <munoz@avant.nl>
2020-01-09 15:48:15 +0100

    extra cool doen

--- Pokemon.php
+++ Pokemon.php
@@ -3,34 +3,72 @@
 class Pokemon
 {
     public $name;
+    public $nickName;
     public $energyType;
     public $hitpoints;
     public $health;
     public $attacks;
-    public $weakness;
+    public $weaknessName;
     public $resistance;
     public $weaknessMultiplier;
     public $resistanceValue;
 
-    public function __construct($name, $energyType, $hitpoints, $health, $attacks, $weakness, $weaknessMultiplier, $resistance, $resistanceValue)
+    public function __construct($name, $nickName, $energyType, $hitpoints, $attacks, $weaknessName, $weaknessMultiplier, $resistance, $resistanceValue)
     {
         $this->name = $name;
+        $this->nickName = $nickName;
         $this->energyType = new EnergyType($energyType);
         $this->hitpoints = $hitpoints;
-        $this->health = $health;
+        $this->health = $hitpoints;
         $this->attacks = $attacks;
-        $this->weakness = new Weakness($weakness, $weaknessMultiplier);
+        $this->weaknessName = new Weakness($weaknessName, $weaknessMultiplier);
         $this->resistance = new Resistance($resistance, $resistanceValue);
     }
 
-    public function attack()
+    public function battleMove($target, $attack)
     {
+        echo "<br><strong> $target->name total hp: $target->health</strong> <br><br>";
+        if ($target->weaknessName->energyType == $this->energyType->name) {
+            $multipliedDamage = $attack[1] * $this->weaknessName->multiplier;
+            echo "$this->name attacked with $attack[0] - will do $multipliedDamage damage <br>";
+        } else {
+            $multipliedDamage = $attack[1];
+            echo "No damage multiplied <br>";
+        }
+        if ($target->resistance->energyType == $this->energyType->name) {
+            $damage = $multipliedDamage - $target->resistance->value;
+            echo "Attack damage has been reduced to $damage damage <br>";
+            echo "$this->name will do $damage damage to $target->name <br>";
+        } else {
+            echo "no resistance stuff <br>";
+            $damage = $multipliedDamage;
+            echo "$this->name will do $multipliedDamage damage to $target->name <br>";
+        }
+
+
+        $this->decreaseHealth($damage, $target);
     }
 
-    public function __toString()
+    public function decreaseHealth($damage, $target)
     {
-        return json_encode($this);
+        $target->health -= $damage;
+        if ($target->health <= 0) {
+            echo "$this->name killed $target->name";
+        } else {
+            echo "$target->name is left with $target->health hp <br>";
+        }
     }
+    // public function getHealth()
+    // {
+    //     return $this->health;
+    // }
+
+    // public function setHealth($hp)
+    // {
+    //     if ($hp > 0) {
+    //         $this->health = $hp;
+    //     }
+    // }
 }
 
 class EnergyType
