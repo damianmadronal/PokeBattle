b1b8c30cad72829b1dd7753e9a0bbeed93bf9bd2
Damian Madronal <munoz@avant.nl>
2020-01-09 11:55:12 +0100

    added 2 pokemons

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 require 'Pokemon.php';
+
+$pikachu = new Pokemon('Pikachu', 'Pickachu', 'Lightning', 60, [['Electric Ring', 50], ['Pika Punch', 20]], 'Fire', 1.5, 'Fighting', 20);
+
+$charmeleon = new Pokemon('Charmeleon', 'Chermelon', 'Fire', 60, [['Head Butt', 10], ['Flare', 30]], 'Water', 2, 'Lightning', 10);
+
+echo '<pre>';
+var_dump($pikachu, $charmeleon);
+echo '</pre>';
+die;
