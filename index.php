f933ae5f1b8013f48d2931b10411c31d32f6c652
Damian Madronal <munoz@avant.nl>
2020-01-09 16:16:14 +0100

    ja

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body style="background-color : #000;color:#fff">
-
-</body>
-
-</html>
 <?php
 
 require 'Pokemon.php';
@@ -34,3 +19,21 @@ while ($pikachu->health > 0 && $charmeleon->health > 0) {
     sleep(3);
 }
 ob_implicit_flush(0);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+</body>
+
+
+</html>
